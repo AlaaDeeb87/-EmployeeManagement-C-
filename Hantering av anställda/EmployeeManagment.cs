ef3f3b24c37555a94c236c3bffab3016c2ab8e7a
Alaa Deeb <71987914+AlaaDeeb87@users.noreply.github.com>
2020-11-22 22:26:03 +0100

    FilePath

--- Hantering av anställda/EmployeeManagment.cs
+++ Hantering av anställda/EmployeeManagment.cs
@@ -11,8 +11,8 @@ namespace Hantering_av_anställda
         //public static readonly string path = @"EmployeeManagement.csv";
 
         //string AppFolder = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData );
-        public static readonly string path = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData));
-        public static readonly string  fileName = @"EmployeeManagement.csv";
+        public static readonly string fileNmae = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData));
+        public static readonly string path = @"EmployeeManagement.csv";
         public void employeeManagement()
         {
             EmployeeManagement obj_Company = new EmployeeManagement();
