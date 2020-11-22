fa4bdf8b1d5dc234e0309acd22db5163ad95aadf
Alaa Deeb <71987914+AlaaDeeb87@users.noreply.github.com>
2020-11-22 15:13:01 +0100

    Path File

--- Hantering av anställda/Employee.cs
+++ Hantering av anställda/Employee.cs
@@ -1,41 +1,56 @@
 ﻿using Collection;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Text;
 
 namespace Hantering_av_anställda
 {
     public class Employee : EmployeeManagement
     {
-          public static void EmployeeRights()
+        public static void EmployeeRights()
         {
             Console.WriteLine("View Employee details type 1 \nModify Employee details type 2");
             int EmpInput = int.Parse(Console.ReadLine());
             EmployeeManagement obj_Company = new EmployeeManagement();
             List<AbcCmpany> employeeList = new List<AbcCmpany>();
-            if (EmpInput==1)
+            if (EmpInput == 1)
             {
-               
-                Console.WriteLine("Enter Employee Id Which You Want To Search:");
-                int search_Id = int.Parse(Console.ReadLine());
-                AbcCmpany obj_search = obj_Company.Function_Search(employeeList, search_Id);
-                if (obj_search != null)
+                using (var inFile = new StreamReader(path))
                 {
-                    Console.WriteLine("Employee ID \t{0}", obj_search.emp_Id);
-                    Console.WriteLine("Employee Name \t{0}", obj_search.emp_Name);
-                    Console.WriteLine("Employee Address \t{0}", obj_search.emp_Address);
-                    Console.WriteLine("Designation \t{0}\n", obj_search.emp_Designation);
-                }
-                else
-                {
-                    Console.WriteLine("Record Not Found...!!!");
+                    string line;
+
+                    while (inFile.Peek() != -1)
+                    {
+                        Console.WriteLine("Enter your ID:");
+                        int search_Id = int.Parse(Console.ReadLine());
+                        AbcCmpany obj_search = obj_Company.Function_Search(employeeList, search_Id);
+                        line = inFile.ReadLine();
+                        Console.WriteLine("****************************Employee Details****************************************");
+                        Console.WriteLine("------------------------------------------------------------------------------------");
+                        Console.WriteLine("Employee Id\tEmployee Name\tEmployee Addess\tEmployee Designation");
+
+                        foreach (AbcCmpany i in employeeList)
+                        {
+                            Console.Write(i.emp_Id + "      \t|  " + i.emp_Name + " \t| " + i.emp_Address + "   \t|  " + i.emp_Designation);
+
+                        }
+                        Console.WriteLine("------------------------------------------------------------------------------------");
+                        Console.WriteLine(line);
+
+                    }
+                    Console.WriteLine();
+                    inFile.Close();
                 }
 
+
+
             }
-            else if (EmpInput==2)
+            else if (EmpInput == 2)
             {
                 Console.WriteLine("Enter Employee Id Which You Want To Search:");
                 int search_Id = int.Parse(Console.ReadLine());
+                StreamReader inFile = new StreamReader(path);
                 AbcCmpany obj_Modify = obj_Company.Function_Search(employeeList, search_Id);
                 if (obj_Modify != null)
                 {
@@ -50,10 +65,10 @@ namespace Hantering_av_anställda
                 {
                     Console.WriteLine("Record Not Found...!!!");
                 }
-
+                inFile.Close();
             }
 
         }
- 
+
     }
 }
--- Hantering av anställda/EmployeeManagment.cs
+++ Hantering av anställda/EmployeeManagment.cs
@@ -6,9 +6,9 @@ using System.Text;
 
 namespace Hantering_av_anställda
 {
-   public class EmployeeManagement
+    public class EmployeeManagement
     {
-        private static readonly string _path = "EmployeeManagement.csv";
+        public static readonly string path = @"EmployeeManagement.csv";
         public void employeeManagement()
         {
             EmployeeManagement obj_Company = new EmployeeManagement();
@@ -41,6 +41,7 @@ namespace Hantering_av_anställda
                         obj_Company.Function_Display_Employee(employeeList);
                         break;
                     case 3:
+
                         Console.WriteLine("Enter Employee Id Which You Want To Search:");
                         search_Id = int.Parse(Console.ReadLine());
                         AbcCmpany obj_search = obj_Company.Function_Search(employeeList, search_Id);
@@ -56,42 +57,55 @@ namespace Hantering_av_anställda
                             Console.WriteLine("Record Not Found...!!!");
                         }
                         break;
+
                     case 4:
+
                         Console.WriteLine("Enter Employee Id Which You Want To Search:");
                         search_Id = int.Parse(Console.ReadLine());
                         AbcCmpany obj_Modify = obj_Company.Function_Search(employeeList, search_Id);
+
                         if (obj_Modify != null)
                         {
+
                             Console.WriteLine("Employee ID      :" + obj_Modify.emp_Id);
                             Console.WriteLine("Employee Name    :" + obj_Modify.emp_Name);
                             Console.WriteLine("Employee Address :" + obj_Modify.emp_Address);
                             Console.WriteLine("Designation      :" + obj_Modify.emp_Designation);
                             obj_Company.Fucntion_Modify_Employee(employeeList, obj_Modify);
                             obj_Company.Function_Display_Employee(employeeList);
+
+
                         }
+
                         else
                         {
                             Console.WriteLine("Record Not Found...!!!");
                         }
 
+
                         break;
                     case 5:
-                        Console.WriteLine("Enter Employee Id Which You Want To Search:");
-                        search_Id = int.Parse(Console.ReadLine());
-                        AbcCmpany obj_Delete = obj_Company.Function_Search(employeeList, search_Id);
-                        if (obj_Delete != null)
+                        int choice;
+                        Function_Display_Employee(employeeList);
+                        Console.Write("Which item do you want to remove? ");
+                        choice = int.Parse(Console.ReadLine());
+                        List<string> items = new List<string>();
+                        int number = 1;
+                        string item;
+                        StreamReader inFile = new StreamReader(path);
+                        while (inFile.Peek() != -1)
                         {
-                            Console.WriteLine("Employee ID      :" + obj_Delete.emp_Id);
-                            Console.WriteLine("Employee Name    :" + obj_Delete.emp_Name);
-                            Console.WriteLine("Employee Address :" + obj_Delete.emp_Address);
-                            Console.WriteLine("Designation      :" + obj_Delete.emp_Designation);
-                            obj_Company.Function_Remove(employeeList, obj_Delete);
-                            obj_Company.Function_Display_Employee(employeeList);
+                            item = inFile.ReadLine();
+                            if (number != choice)
+                                items.Add(item);
+                            ++number;
                         }
-                        else
-                        {
-                            Console.WriteLine("Record Not Found...!!!");
-                        }
+                        inFile.Close();
+                        StreamWriter outFile = new StreamWriter(path);
+                        for (int i = 0; i < items.Count; i++)
+                            outFile.WriteLine(items[i]);
+                        outFile.Close();
+
                         break;
                     case 6:
                         Environment.Exit(0);
@@ -105,97 +119,126 @@ namespace Hantering_av_anställda
             } while (ans == 'y' || ans == 'Y');
         }
 
-      
+
 
         public void Function_Add_Employee(List<AbcCmpany> employeeList)
         {
-            StreamWriter outFile = File.AppendText(_path);
+            StreamWriter outFile = File.AppendText(path);
             AbcCmpany obj_Comapny1 = new AbcCmpany();
-            Console.Write("Enter Employee Id:");
+            Console.Write("Enter Employee Id:\n");
             obj_Comapny1.emp_Id = int.Parse(Console.ReadLine());
-            outFile.WriteLine(obj_Comapny1.emp_Id);
-            Console.Write("Enter Employee Name:");
+            outFile.Write(obj_Comapny1.emp_Id + "              ");
+            Console.Write("Enter Employee Name:\n");
             obj_Comapny1.emp_Name = Console.ReadLine();
-            outFile.WriteLine(obj_Comapny1.emp_Name);
-            Console.Write("Enter Employee Addess:");
+            outFile.Write(obj_Comapny1.emp_Name + "              ");
+            Console.Write("Enter Employee Addess:\n");
             obj_Comapny1.emp_Address = Console.ReadLine();
-            outFile.WriteLine(obj_Comapny1.emp_Address);
-            Console.Write("Enter Employee Designation:");
+            outFile.Write(obj_Comapny1.emp_Address + "              ");
+            Console.Write("Enter Employee Designation:\n");
             obj_Comapny1.emp_Designation = Console.ReadLine();
-            outFile.WriteLine(obj_Comapny1.emp_Designation);
+            outFile.Write(obj_Comapny1.emp_Designation);
             employeeList.Add(obj_Comapny1);
             Console.WriteLine("Employee Deatil Added Successfully...!!!!:");
+            outFile.WriteLine("");
+
             outFile.Close();
 
         }
 
         public void Function_Display_Employee(List<AbcCmpany> employeeList)
         {
-            Console.WriteLine("****************************Employee Details****************************************");
-            Console.WriteLine("------------------------------------------------------------------------------------");
-            Console.WriteLine("Employee Id\tEmployee Name\tEmployee Addess\tEmployee Designation");
-            Console.WriteLine("------------------------------------------------------------------------------------");
-            foreach (AbcCmpany i in employeeList)
+
+            using (var inFile = new StreamReader(path))
             {
-                Console.WriteLine(i.emp_Id + "      \t|  " + i.emp_Name + " \t| " + i.emp_Address + "   \t|  " + i.emp_Designation);
+                string line;
+
+                while (inFile.Peek() != -1)
+                {
+                    line = inFile.ReadLine();
+                    Console.WriteLine("****************************Employee Details****************************************");
+                    Console.WriteLine("------------------------------------------------------------------------------------");
+                    Console.WriteLine("Employee Id\tEmployee Name\tEmployee Addess\tEmployee Designation");
+
+                    foreach (AbcCmpany i in employeeList)
+                    {
+
+                        Console.Write(i.emp_Id + "      \t|  " + i.emp_Name + " \t| " + i.emp_Address + "   \t|  " + i.emp_Designation);
+
+                    }
+                    Console.WriteLine("------------------------------------------------------------------------------------");
+                    Console.WriteLine(line);
+
+                }
+
+                inFile.Close();
             }
-            Console.WriteLine("------------------------------------------------------------------------------------");
+
         }
 
         public AbcCmpany Function_Search(List<AbcCmpany> employeeList, int search_Id)
         {
             return employeeList.Find(emp => emp.emp_Id == search_Id);
+
         }
 
 
         public void Fucntion_Modify_Employee(List<AbcCmpany> employeeList, AbcCmpany obj_Modify)
         {
+
             Console.WriteLine("Chose Option for Modify Employee Detail:");
             Console.WriteLine("1.Id 2.Name 3.Address 4.Designation");
-            StreamWriter outFile = File.AppendText(_path);
+            StreamWriter outFile = File.AppendText(path);
             int modify_number = int.Parse(Console.ReadLine());
+
+
+
             switch (modify_number)
             {
                 case 1:
+
                     Console.WriteLine("Enter New Employee Id:");
                     int new_Id = int.Parse(Console.ReadLine());
+                    outFile.Write(new_Id + "              ");
                     obj_Modify.emp_Id = new_Id;
                     break;
                 case 2:
                     Console.WriteLine("Enter New Employee Name:");
                     string new_Name = Console.ReadLine();
+                    outFile.Write(new_Name + "              ");
                     obj_Modify.emp_Name = new_Name;
                     break;
                 case 3:
                     Console.WriteLine("Enter New Employee Address:");
                     string new_Address = Console.ReadLine();
+                    outFile.Write(new_Address + "              ");
                     obj_Modify.emp_Address = new_Address;
                     break;
                 case 4:
                     Console.WriteLine("Enter New Employee Designation:");
                     string new_Designation = Console.ReadLine();
+                    outFile.Write(new_Designation + "              ");
                     obj_Modify.emp_Designation = new_Designation;
                     break;
                 default:
                     Console.WriteLine("Invalide Choise....");
                     break;
+
             }
-            outFile.WriteLine(obj_Modify);
+
             outFile.Close();
-            
+
         }
 
         public void Function_Remove(List<AbcCmpany> employeeList, AbcCmpany obj_Modify)
         {
+
             employeeList.Remove(obj_Modify);
             Console.WriteLine("1 Record Removed SuccessFully....!!!");
-            StreamWriter outFile = new StreamWriter(_path);
-            outFile.WriteLine(obj_Modify);
-            outFile.Close();
-            
+
+
         }
-        
+
     }
 
-    
+
 }
